a6f0d9a354fba61d59b6009503a4efb426f04072
AmitPirannavarAP <amit.pirannavar@lr.org>
2025-07-04 18:57:01 +0530

    Modified

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 from fastapi.testclient import TestClient
-from bookstore.main import app
+import bookstore.main
 from bookstore.database import SQLModel, engine, SessionLocal
 
 @pytest.fixture(scope="module")
 def client():
     SQLModel.metadata.create_all(engine)
-    with TestClient(app) as c:
+    with TestClient(bookstore.main.app) as c:
         yield c
 
 @pytest.fixture(scope="function")
